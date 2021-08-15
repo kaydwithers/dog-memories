7eaa2cc9d3d45ff3d6cbd3a1ca9f0d23d56e0637
kaydwithers <kayd.withers@isobar.com>
2021-08-15 18:44:33 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rick and Morty',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
